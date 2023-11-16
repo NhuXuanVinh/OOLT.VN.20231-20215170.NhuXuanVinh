f90c6fecd6e7fcfebfcf0b0b350596ca9c084fe3
NhuXuanVinh <vinhnhu30@gmail.com>
2023-11-17 00:56:11 +0700

    lab3

--- Lab1/src/Test.java
+++ Lab1/src/Test.java
@@ -1,5 +1,9 @@
 public class Test {
     public static void main(String[] args){
-        String a
+        int x=9;
+        int y=5;
+        if(x=y){
+            System.out.println(x);
+        }
     }
 }
